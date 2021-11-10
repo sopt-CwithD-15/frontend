d1ac2ab58293e4705f4b232965489bb1818ab39f
KimKwon <khj9709@hanyang.ac.kr>
2021-11-11 00:30:01 +0900

    init: :tada: 개발환경세팅(1)

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/* "prettier/prettier": ["error", { endOfLine: "auto" }, { usePrettierrc: true }],
+윈도우 유저가 개행 CRLF 에러 나면 rules에 추가
+*/
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/display-name": "off",
+  },
+};
