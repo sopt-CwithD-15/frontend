f4f066a6e4283ab2d8c7f08c463957ff379ac728
KimKwon <khj9709@hanyang.ac.kr>
2021-11-12 02:11:43 +0900

    is Github action work?

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    <div class"App">
+      <header class="App-header">dwqdwqdqwdmwqkldhttps://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
+        </a>idqwjioqwdm
       </header>
     </div>
   );
