6dea3ebe7faa9b7d753715bb4beaaf0c04e83c41
KimKwon <khj9709@hanyang.ac.kr>
2021-12-01 13:37:25 +0900

    feat: :sparkles: 서버 attach를 위한 test api 생성

---
+++ src/Cores/Hooks/useTest.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import { test } from 'Cores/api';
+
+function useAPI(apiInfo) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState(null);
+  const [shouldUpdate, setShouldUpdate] = useState(0);
+  const mutate = () => setShouldUpdate((prev) => prev + 1);
+
+  useEffect(() => {
+    async function execAPICall() {
+      try {
+        setLoading(true);
+        const result = await test.request(apiInfo);
+        setData(result.data);
+      } catch (error) {
+        setError({
+          message: error,
+        });
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    execAPICall();
+  }, [shouldUpdate]);
+
+  return { data, loading, error, mutate };
+}
+
+export default useAPI;
