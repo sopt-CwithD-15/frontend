a9c15ee2937b911b21cec86ef50284215969d246
KimKwon <khj9709@hanyang.ac.kr>
2021-12-02 01:43:55 +0900

    chore: :clipboard: Icon Asset 추가

---
+++ src/Constants/sidebar-list.js
@@ -0,0 +1,118 @@
+import { ReactComponent as FinderIC } from 'Assets/icon/Filterbar/finderIC.svg';
+import { ReactComponent as ContainerIC } from 'Assets/icon/Sidebar/container.svg';
+import { ReactComponent as RecentIC } from 'Assets/icon/Sidebar/recent.svg';
+import { ReactComponent as SubscribeIC } from 'Assets/icon/Sidebar/subscribe.svg';
+import { ReactComponent as HomeIC } from 'Assets/icon/Sidebar/home.svg';
+import { ReactComponent as MusicIC } from 'Assets/icon/Sidebar/music.svg';
+import { ReactComponent as SportsIC } from 'Assets/icon/Sidebar/sports.svg';
+import { ReactComponent as GameIC } from 'Assets/icon/Sidebar/game.svg';
+import { ReactComponent as MovieIC } from 'Assets/icon/Sidebar/movie.svg';
+import { ReactComponent as NewsIC } from 'Assets/icon/Sidebar/news.svg';
+import { ReactComponent as RealtimeBgIC } from 'Assets/icon/Sidebar/realtime-bg.svg';
+import { ReactComponent as EducationIC } from 'Assets/icon/Sidebar/education.svg';
+import { ReactComponent as VrIC } from 'Assets/icon/Sidebar/vr.svg';
+import { ReactComponent as PremiumIC } from 'Assets/icon/Sidebar/youtube-premium.svg';
+import { ReactComponent as RealtimeIC } from 'Assets/icon/Sidebar/realtime.svg';
+import { ReactComponent as SettingIC } from 'Assets/icon/Sidebar/setting.svg';
+import { ReactComponent as ReportIC } from 'Assets/icon/Sidebar/report-history.svg';
+import { ReactComponent as ServiceIC } from 'Assets/icon/Sidebar/service-center.svg';
+import { ReactComponent as OpinionIC } from 'Assets/icon/Sidebar/send-opinion.svg';
+import { ReactComponent as ChannelIC } from 'Assets/icon/Sidebar/channel-search.svg';
+
+export const mobileList = [
+  {
+    label: '홈',
+    icon: HomeIC,
+  },
+  {
+    label: '탐색',
+    icon: FinderIC,
+  },
+  {
+    label: '구독',
+    icon: SubscribeIC,
+  },
+];
+
+export const storageList = [
+  {
+    label: '보관함',
+    icon: ContainerIC,
+  },
+  {
+    label: '시청 기록',
+    icon: RecentIC,
+  },
+];
+
+export const famousYoutube = [
+  {
+    label: '음악',
+    icon: MusicIC,
+  },
+  {
+    label: '스포츠',
+    icon: SportsIC,
+  },
+  {
+    label: '게임',
+    icon: GameIC,
+  },
+  {
+    label: '영화',
+    icon: MovieIC,
+  },
+  {
+    label: '뉴스',
+    icon: NewsIC,
+  },
+  {
+    label: '실시간',
+    icon: RealtimeBgIC,
+  },
+  {
+    label: '학습',
+    icon: EducationIC,
+  },
+  {
+    label: '360° 동영상',
+    icon: VrIC,
+  },
+];
+
+export const channelSearch = [
+  {
+    label: '채널 탐색',
+    icon: ChannelIC,
+  },
+];
+
+export const youtubeMore = [
+  {
+    label: 'YouTube Premium',
+    icon: PremiumIC,
+  },
+  {
+    label: '실시간',
+    icon: RealtimeIC,
+  },
+];
+
+export const lastTabs = [
+  {
+    label: '설정',
+    icon: SettingIC,
+  },
+  {
+    label: '신고 기록',
+    icon: ReportIC,
+  },
+  {
+    label: '고객센터',
+    icon: ServiceIC,
+  },
+  {
+    label: '의견 보내기',
+    icon: OpinionIC,
+  },
+];
